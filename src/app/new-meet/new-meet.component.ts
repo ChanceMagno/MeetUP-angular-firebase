cf72ee2cd5356239f7dfd11c3e62f0f449648e59
Chance <ChanceMagno@gmail.com>
2017-05-12 20:28:17 -0700

    add empty form field check method to new meet component, add modal to new meet html

--- src/app/new-meet/new-meet.component.ts
+++ src/app/new-meet/new-meet.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import {MaterializeAction} from 'angular2-materialize';
 import { Meet } from '../meet.model';
 import { MeetService } from '../meet.service';
+import { MaterializeModule } from "angular2-materialize";
 
 @Component({
   selector: 'app-new-meet',
@@ -13,6 +14,7 @@ export class NewMeetComponent implements OnInit {
   newMeetForm: FormGroup;
   selectedGoal: number;
 
+  warnAction = new EventEmitter<string|MaterializeAction>();
   constructor(private fb: FormBuilder, private MeetService: MeetService) { }
 
 
@@ -27,13 +29,24 @@ export class NewMeetComponent implements OnInit {
       image: ['', Validators.required],
       attendingGoal: ['', Validators.required],
     })
+  }
 
+  warnModal() {
+  this.warnAction.emit({action:"modal",params:['open']});
+  }
+
+  showGoal(goal){
+    this.selectedGoal = goal;
   }
 
   addMeet() {
     var {title, location, info, time, date, creator, image, attendingGoal} = this.newMeetForm.value;
-    var newMeet = new Meet(title, location, time, date, creator, image, info, attendingGoal);
-    this.MeetService.saveMeet(newMeet);
-    this.newMeetForm.reset();
+    if(title.length < 2 || location.length < 2 || info.length < 2 || time.length < 2 || date.length < 2 || creator.length < 2 || image.length < 2 || attendingGoal.length < 1) {
+      this.warnModal();
+    } else{
+      var newMeet = new Meet(title, location, time, date, creator, image, info, attendingGoal);
+      this.MeetService.saveMeet(newMeet);
+      this.newMeetForm.reset();
+      }
     }
   }
